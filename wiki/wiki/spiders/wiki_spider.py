bec92550e7e5bf7f9eba999dd0b2088b1c3e23c3
Pedro <pedro@Pedros-MacBook-Air-3.local>
2020-02-03 16:07:08 -0800

    modified wiki spider to collect url,outgoing and incoming links

--- wiki/wiki/spiders/wiki_spider.py
+++ wiki/wiki/spiders/wiki_spider.py
@@ -3,40 +3,68 @@ import re
 from bs4 import BeautifulSoup
 
 
+incomingLinks = {}
+
 class EDUSpider(scrapy.Spider):
     name = 'Wiki'
 
     start_urls = ['https://en.wikipedia.org/wiki/Marvel_Comics']
 
     def parse(self, response):
-        # follow links to author pages
+        #Change response information to soup context
         soup = BeautifulSoup(response.body, 'html.parser')
 
+        #Get the title and last edit date of the current page
         title = soup.h1.get_text()
         date = response.xpath('//li[@id="footer-info-lastmod"]/text()').get()
-        table = soup.table
 
+        #Get the right table facts of the page
+        table = soup.table
         if table == None:
             return
-
         facts = soup.table.get_text()
 
+        #Get the external website of the page
         websiteList = response.css('tr span.url a::text').getall()
         website = ""
         for part in websiteList:
             website = website + part
 
-
+        #Get all the paragraphs in the page
         p = table.find_all_next("p")
         psoup = BeautifulSoup(str(p),features="lxml")
+
+        #count the number of outgoing links
+        outGoingLinks = 0
+        for link in psoup.find_all('a'):
+            url = str(link.get('href'))
+            if re.search(r'\/wiki\/[0-9A-Za-z_-]+$',url):
+                outGoingLinks = outGoingLinks + 1
+                if url in incomingLinks:
+                    incomingLinks[url] = incomingLinks[url] + 1
+                else:
+                    incomingLinks.update({url:1})
+
+        url = response.request.url
+        short_url = url.replace('https://en.wikipedia.org','')
+        if short_url in incomingLinks:
+            incomingLinks[short_url] = incomingLinks[short_url] + 1
+        else:
+            incomingLinks.update({short_url:1})
+        #yield (store) all the information
         yield {
             'title': title,
             'lastEdit': date,
+            'url': response.request.url,
             'facts': facts,
             'website': website,
+            'outGoingLinks': outGoingLinks,
+            'incomingLinks': incomingLinks[short_url],
             'description' : psoup.get_text()
         }
 
+        #Get all links within the paragraphs and schedule the correct incomingLinks
+        # with the following format "http://en.wikipedia.org/wiki/<page here>"
         for link in psoup.find_all('a'):
             url = str(link.get('href'))
             if re.search(r'\/wiki\/[0-9A-Za-z_-]+$',url):
