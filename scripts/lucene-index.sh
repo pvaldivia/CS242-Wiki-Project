533551228c62b20b7b9b8a4faf77e775bce02ece
Justin Schopick <jscho004@ucr.edu>
2020-02-07 20:48:14 -0800

    Updated lucene script

--- scripts/lucene-index.sh
+++ scripts/lucene-index.sh
@@ -4,10 +4,12 @@
 # On linux: sudo apt install maven
 # You'll need to set your JAVA_HOME properly.
 
+cd ../LucenePartA
 echo "Building Project Files"
 sleep 2
 mvn package
 
+echo ""
 echo "Building Lucene Index"
-sleep 2
+sleep 4
 mvn exec:java -Dexec.mainClass="edu.ucr.cs.cs242.group14.LucenePartA" -Dexec.args="./index ./data \"marvel villans\" 1"
